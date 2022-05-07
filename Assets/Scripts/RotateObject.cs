c498ec8e6b1bc4c862ae601fe5c15d6adbbcd417
LudovicRousselle <lrousselle@artfx.fr>
2022-05-07 19:58:34 +0200

    Initial commit

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] Camera cam;
+
+
+
+    private void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotationSpeed;
+        float rotY = Input.GetAxis("Mouse Y") *rotationSpeed;
+
+        Vector3 right = Vector3.Cross(cam.transform.up, transform.position = cam.transform.position);
+        Vector3 up = Vector3.Cross(transform.position - cam.transform.position, right);
+
+        transform.rotation = Quaternion.AngleAxis(-rotX, up) * transform.rotation;
+        transform.rotation = Quaternion.AngleAxis(rotY, right) * transform.rotation;
+    }
+}
